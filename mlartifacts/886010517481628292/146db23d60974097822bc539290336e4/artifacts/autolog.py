dfbb85a90080810d02fd034c13d0765a0fcb38bc
Biraja prasad Barik <kbarik153@gmail.com>
2025-10-26 13:28:09 +0530

    updated till mlflow

---
+++ mlartifacts/886010517481628292/146db23d60974097822bc539290336e4/artifacts/autolog.py
@@ -0,0 +1,57 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score , confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+#load wine data set
+wine=load_wine()
+x=wine.data
+y=wine.target
+
+#train test split
+X_train,X_test,y_train,y_test=train_test_split(x, y, test_size=0.10, random_state=42)
+
+#define params for rf model
+max_depth=10
+n_estimators=5
+
+#Mention ur experiment name
+mlflow.autolog()
+mlflow.set_experiment('MLops-exp1')
+
+with mlflow.start_run():
+    rf=RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train,y_train)
+    
+    y_pred=rf.predict(X_test)
+    accuracy=accuracy_score(y_test,y_pred)
+    
+    
+    
+    #creating confusion matrix
+    cm=confusion_matrix(y_test,y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm,annot=True, fmt='d' ,cmap='Blues',xticklabels=wine.target_names,yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+    
+    #save plot
+    plt.savefig("Confusion-matrix.png")
+    
+    #log artifacts using mlflow
+    
+    mlflow.log_artifact(__file__)
+    
+    #tags
+mlflow.set_tags({"Author":"Biraja","Project":"Wine Classification"})
+    
+    #Log the model
+mlflow.sklearn.log_model(rf,"Random-forest-model")
+    
+print(accuracy) 
---
+++ mlartifacts/886010517481628292/5e106f12d0a843519d9372975ff29bce/artifacts/file1.py
@@ -0,0 +1,58 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score , confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+#load wine data set
+wine=load_wine()
+x=wine.data
+y=wine.target
+
+#train test split
+X_train,X_test,y_train,y_test=train_test_split(x, y, test_size=0.10, random_state=42)
+
+#define params for rf model
+max_depth=10
+n_estimators=5
+
+#Mention ur experiment name
+mlflow.set_experiment('MLops-exp1')
+
+with mlflow.start_run():
+    rf=RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train,y_train)
+    
+    y_pred=rf.predict(X_test)
+    accuracy=accuracy_score(y_test,y_pred)
+    
+    mlflow.log_metric('accuracy',accuracy)
+    mlflow.log_param('max_depth',max_depth)
+    mlflow.log_param('n_estimators',n_estimators)
+    
+    #creating confusion matrix
+    cm=confusion_matrix(y_test,y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm,annot=True, fmt='d' ,cmap='Blues',xticklabels=wine.target_names,yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+    
+    #save plot
+    plt.savefig("Confusion-matrix.png")
+    
+    #log artifacts using mlflow
+    mlflow.log_artifact("Confusion-matrix.png")
+    mlflow.log_artifact(__file__)
+    
+    #tags
+mlflow.set_tags({"Author":"Biraja","Project":"Wine Classification"})
+    
+    #Log the model
+mlflow.sklearn.load_model(rf,"Random-forest-model")
+    
+print(accuracy) 
